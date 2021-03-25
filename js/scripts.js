5294e0bbb183f64f3cb0cd143dfd6c3646add953
Maxwell Meyer <maxreadswell@gmail.com>
2021-03-25 11:18:50 -0700

    Add imgs, page content, and js functionality to show and hide animal sections on btn click.
    Co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".Turtles").click(function() {
+    $("#turtle").fadeToggle();
+  })
+  $(".Snakes").click(function() {
+    $("#snake").fadeToggle();
+  })
+  $(".Bears").click(function() {
+    $("#bear").fadeToggle();
+  })
+});
